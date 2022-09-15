9750c5318a98ab0fc53dc76faf8821dd5193c3bd
nczimmerman00 <n.zman@eaglecom.net>
2022-09-15 13:43:25 -0500

    Added datetimes as xaxis, axis labels and title

--- main.py
+++ main.py
@@ -6,22 +6,42 @@
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import os
+from datetime import datetime
 
+# These are the tickers to be looked up and graphed.
 tickers = ['MSFT', 'AAPL', 'IBM', 'GOOGL', 'AMZN']
+
+# Set dateFormatter to be used in the xaxis
+dateFormatter = mdates.DateFormatter('%Y-%m-%d')
 
 for ticker in tickers:
     # Request ticker data
     data = yf.download(ticker, start='2022-08-30', end='2022-09-14')
 
-    # Grab ticker closing prices
+    # Grab ticker closing prices and dates
     dataPrices = []
+    dataDates = []
     for price in data['Adj Close']:
         dataPrices.append(price)
 
-    # Create numpy array and plot it
-    dataArray = np.array(dataPrices)
-    plt.plot(dataArray)
+    for date in data.axes[0].date:
+        dataDates.append(date)
+
+    # Create numpy arrays for the prices and dates and plot it
+    priceArray = np.array(dataPrices)
+    dateArray = np.array(dataDates)
+
+    # Edit graph settings
+    plt.clf()
+    plt.title(ticker)
+    plt.xlabel('Date')
+    plt.ylabel('Price ($) per Share')
+    plt.xticks(dateArray, rotation=60)
+    plt.subplots_adjust(bottom=0.25)
+    plt.gca().xaxis.set_major_formatter(dateFormatter)
+    plt.plot(dateArray, priceArray)
 
     # Check to make sure the charts directory is created.
     pathString = os.getcwd() + '/charts/'
@@ -31,3 +51,5 @@ for ticker in tickers:
     # Save graph in charts folder
     pathString += ticker + '.png'
     plt.savefig(pathString)
+
+exit()
